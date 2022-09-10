a193cdca4f002a5bc44bab7badbe0223093366ba
Quoc Anh Bui <anhbq.it97@gmail.com>
2022-09-10 17:20:47 +0700

    udpate

---
+++ resources/js/vue/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script>
+import DashboardJs from '../assets/dashboard.js';
+import CommonJs from '../assets/common.js';
+
+export default {
+    
+}
+</script>
